cc8eaf4743eb3908df3181c9c40cf40701e3c077
maynarasanto <maynara999@gmail.com>
2024-01-30 21:17:57 -0300

    funcionando

--- jogo.js
+++ jogo.js
@@ -1,31 +1,28 @@
 
 function jogos() {
-    
-    let  chuteDojogador=0
-    
-    chuteDojogador = chuteDojogador +1;
-  
-    chuteDojogador = (prompt("Digite seu chute!"));
+let chuteDojogador = 1
 let numeroMisterioso = Math.floor(Math.random() * 101);
 alert(numeroMisterioso)
-
-
-
-
-            if (chuteDojogador > numeroMisterioso) {
-              
-                alert("seu numero é menor que o numero misterioso");
-                
-                
-            } else if (chuteDojogador < numeroMisterioso) {
-               
-                alert("seu numero é maior que o numero misterioso")
-                
-            } else if (chuteDojogador == numeroMisterioso) {
-                alert("vitoria")
-            } 
-            }
-
+while (chuteDojogador <= 101) {
+    
+    chuteDojogador = (prompt("Digite seu chute!"));
+    
+    
+    if (chuteDojogador < numeroMisterioso) {
+        
+        alert("seu numero é menor que o numero misterioso");
+        
+        
+    } else if (chuteDojogador > numeroMisterioso) {
+        
+        alert("seu numero é maior que o numero misterioso")
+        
+    } else if (chuteDojogador == numeroMisterioso) {
+        alert("vitoria")
+    }
+}
+++chuteDojogador
+} 
 
 
 
